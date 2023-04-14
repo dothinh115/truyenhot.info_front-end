0d6345b42ae241423a8ed6d2c8e453faf76feea2
dothinh115 <dothinh115@gmail.com>
2023-04-14 22:43:46 +0700

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { MainLayout } from "@/layouts";
 import { AppPropsWithLayout } from "@/models";
-import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? MainLayout;
