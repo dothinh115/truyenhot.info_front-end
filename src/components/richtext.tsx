b929b4254cdfaa097b4a3c3ac2c46497ed8241eb
dothinh115 <dothinh115@gmail.com>
2023-05-23 18:25:30 +0700

    update

--- src/components/richtext.tsx
+++ src/components/richtext.tsx
@@ -1,9 +1,6 @@
-import { Box, alpha, Stack, IconButton, Button } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { Box, Button, alpha } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import FormatBoldIcon from "@mui/icons-material/FormatBold";
-import FormatItalicIcon from "@mui/icons-material/FormatItalic";
-import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import { useEffect, useRef } from "react";
 type Props = {
   cb: (data: string) => void;
   clicked: boolean;
@@ -28,16 +25,6 @@ const ContentEditableStyled = styled(Box)(({ theme }) => ({
   },
 }));
 
-const IconButtonStyled = styled(Button)(({ theme }) => ({
-  width: "30px",
-  height: "30px",
-  padding: "4px",
-  minWidth: "unset",
-  "& > svg": {
-    fontSize: "1.3em",
-  },
-}));
-
 export const RichTextEditor = ({
   cb,
   clicked,
@@ -45,9 +32,6 @@ export const RichTextEditor = ({
   placeholder,
   defaultValue,
 }: Props) => {
-  const [bold, setBold] = useState<boolean>(false);
-  const [italic, setItalic] = useState<boolean>(false);
-  const [underline, setUnderline] = useState<boolean>(false);
   const contentDiv = useRef<HTMLDivElement>();
   const keyDown = (event: any) => {
     if (event.key === "Enter" || event.keyCode === 13) {
@@ -79,57 +63,6 @@ export const RichTextEditor = ({
 
   return (
     <>
-      <Stack direction={"row"} gap={"5px"} mb={"4px"} width={"100%"}>
-        <IconButtonStyled
-          variant={bold ? "contained" : "text"}
-          size="small"
-          onTouchStart={() => {
-            document.execCommand("bold");
-            setBold(!bold);
-          }}
-          onClick={() => {
-            if (window.innerWidth > 428) {
-              document.execCommand("bold");
-              setBold(!bold);
-            }
-          }}
-        >
-          <FormatBoldIcon />
-        </IconButtonStyled>
-        <IconButtonStyled
-          variant={italic ? "contained" : "text"}
-          size="small"
-          onTouchStart={() => {
-            document.execCommand("italic");
-            setItalic(!italic);
-          }}
-          onClick={() => {
-            if (window.innerWidth > 428) {
-              document.execCommand("italic");
-              setItalic(!italic);
-            }
-          }}
-        >
-          <FormatItalicIcon />
-        </IconButtonStyled>
-        <IconButtonStyled
-          variant={underline ? "contained" : "text"}
-          size="small"
-          onTouchStart={() => {
-            document.execCommand("underline");
-            setUnderline(!underline);
-          }}
-          onClick={() => {
-            if (window.innerWidth > 428) {
-              document.execCommand("underline");
-              setUnderline(!underline);
-            }
-          }}
-        >
-          <FormatUnderlinedIcon />
-        </IconButtonStyled>
-      </Stack>
-
       <Box flexGrow={1}>
         <ContentEditableStyled
           contentEditable={true}
