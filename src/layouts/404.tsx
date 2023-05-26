e5243dbe6944fd5b35ef4332265987596cec2c16
dothinh115 <dothinh115@gmail.com>
2023-05-26 17:16:18 +0700

    update 404 page

---
+++ src/layouts/404.tsx
@@ -0,0 +1,14 @@
+import { useDidMount } from "@/hooks";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export const Layout404 = () => {
+  const didMount = useDidMount();
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  });
+
+  if (!didMount) return null;
+};
---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Layout404 } from "@/layouts/404";
+
+const Custom404 = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  });
+
+  return null;
+};
+
+Custom404.Layout = Layout404;
+
+export default Custom404;
